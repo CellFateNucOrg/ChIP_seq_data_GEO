bac7982b3bce4c8a5e8a4e14357a8086e6c0b5ed
meisterpeter <peter.meister@izb.unibe.ch>
2020-06-29 12:31:12 +0200

    Added file locations source

---
+++ file_locations.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+trim_galore_location=/data/projects/p025/Peter/software/TrimGalore-0.6.5/
+export trim_galore_location=$trim_galore_location
+genome_folder=/data/projects/p025/Peter/ChIP_seq/genome/
+export genome_folder=$genome_folder
