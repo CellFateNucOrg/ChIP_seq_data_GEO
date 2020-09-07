35963c7120b4e45a5a55bba5b17bb3c4a65a2de4
meisterpeter <peter.meister@izb.unibe.ch>
2020-09-07 11:12:44 +0200

    Changed genome location in the file_locations_examples.sh

--- file_locations_example.sh
+++ file_locations_example.sh
@@ -2,4 +2,4 @@
 trim_galore_location=/data/projects/p025/Peter/software/TrimGalore-0.6.5/
 export trim_galore_location=$trim_galore_location
 genome_location=/data/projects/p025/Peter/ChIP_seq/genome/
-export genome_location=$genome_folder
+export genome_location=$genome_location
