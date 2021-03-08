ef265e63ed9fbd43cdba814122e9994fa393ebfa
jsemple19 <semple.j@gmail.com>
2021-03-08 23:44:03 +0100

    SRR table of modified histones from modencode

--- modEncode_modHistone/getSRRfromGSM.R
+++ modEncode_modHistone/getSRRfromGSM.R
@@ -2,6 +2,9 @@ library(reutils)
 library(xml2)
 library(stringr)
 
+projDir<-getwd()
+workDir<-paste0(projDir,"/modEncode_modHistone")
+
 
 #esearch -db sra -query "GSM123456" | efetch -format docsum | xtract -pattern DocumentSummary -element Runs | perl -ne '@mt = ($_ =~ /SRR\d+/g); print "@mt\n"'`
 #
@@ -55,9 +58,10 @@ processGeoNumList<-function(geoNums){
 
 
 
-geoTable<-read.csv(file="modEncode_chromatinChipSeq_modHistone.csv",header=T, stringsAsFactors=F)
+geoTable<-read.csv(file=paste0(workDir,"/modEncode_chromatinChipSeq_modHistone.csv"),
+                   header=T, stringsAsFactors=F)
 #geoTable<-data.frame(readxl::read_excel("modEncode_chromatinChipSeq_modHistone.xlsx", 3))
-
+geoTable$GEO<-sapply(geoTable$GEO,function (x) gsub("^\\s+|\\s+$", "", x))
 geoNums<-unlist(sapply(geoTable$GEO,strsplit,split=";"),use.names=F)
 geoNums<-na.omit(geoNums)
 srrTable<-processGeoNumList(geoNums)
@@ -67,40 +71,105 @@ srrTable[rowSums(is.na(srrTable))>1,]
 srrTable[(srrTable$input + srrTable$ip)!=1,]
 srrTable[(srrTable$rep1 + srrTable$rep2)!=1,]
 
-mergeGeoSrrTables<-function(geoTable,srrTable){
-  geoTable$input<-NA
-  geoTable$ip<-NA
-  geoTable$name_1<-NA
-  geoTable$genotype_1<-NA
-  geoTable$strain_1<-NA
-  geoTable$stage_1<-NA
-  geoTable$sex_1<-NA
-  input1<-srrTable$input & srrTable$rep1
-  input2<-srrTable$input & srrTable$rep2
-  ip1<-srrTable$ip & srrTable$rep1
-  ip2<-srrTable$ip & srrTable$rep2
+# mergeGeoSrrTables<-function(geoTable,srrTable){
+#   geoTable$input<-NA
+#   geoTable$ip<-NA
+#   geoTable$name_1<-NA
+#   geoTable$genotype_1<-NA
+#   geoTable$strain_1<-NA
+#   geoTable$stage_1<-NA
+#   geoTable$sex_1<-NA
+#   input1<-srrTable$input & srrTable$rep1
+#   input2<-srrTable$input & srrTable$rep2
+#   ip1<-srrTable$ip & srrTable$rep1
+#   ip2<-srrTable$ip & srrTable$rep2
+#   for(i in 1:nrow(geoTable)){
+#     geoNums<-geoTable$GEO[i]
+#     geoNums<-unlist(stringr::str_split(geoNums,";"))
+#     idx<-match(geoNums,srrTable$GEO)
+#     geoTable$input[i]<-paste0(
+#       c(srrTable$SRR[idx][input1[idx]],
+#         srrTable$SRR[idx][input2[idx]]), collapse=";")
+#     geoTable$ip[i]<-paste0(
+#       c(srrTable$SRR[idx][ip1[idx]],
+#         srrTable$SRR[idx][ip2[idx]]), collapse=";")
+#     geoTable$name_1[i]<-paste0(c(srrTable$name[idx], collapes=";"))
+#     geoTable$genotype_1[i]<-srrTable$genotype[idx][1]
+#     geoTable$strain_1[i]<-srrTable$strain[idx][1]
+#     geoTable$stage_1[i]<-srrTable$stage[idx][1]
+#     geoTable$sex_1[i]<-srrTable$sex[idx][1]
+#     geoNums<-NULL
+#   }
+#   return(geoTable)
+# }
+
+
+mergeGeoSrrTables1<-function(geoTable,srrTable){
+  newTable<-NULL
+  failed<-c()
   for(i in 1:nrow(geoTable)){
     geoNums<-geoTable$GEO[i]
     geoNums<-unlist(stringr::str_split(geoNums,";"))
+
+    dfline<-cbind(geoTable[i,], data.frame(input_1=NA, ip_1=NA, name_1=NA,
+                                           genotype_1=NA, strain_1=NA,
+                                           stage_1=NA, sex_1=NA, replicate_1=NA,
+                                           group_1=i))
+    dflines<-as.data.frame(do.call("rbind",replicate(length(geoNums)/2,dfline,simplify=F)))
+
     idx<-match(geoNums,srrTable$GEO)
-    geoTable$input[i]<-paste0(
-      c(srrTable$SRR[idx][input1[idx]],
-        srrTable$SRR[idx][input2[idx]]), collapse=";")
-    geoTable$ip[i]<-paste0(
-      c(srrTable$SRR[idx][ip1[idx]],
-        srrTable$SRR[idx][ip2[idx]]), collapse=";")
-    geoTable$name_1[i]<-srrTable$name[idx][1]
-    geoTable$genotype_1[i]<-srrTable$genotype[idx][1]
-    geoTable$strain_1[i]<-srrTable$strain[idx][1]
-    geoTable$stage_1[i]<-srrTable$stage[idx][1]
-    geoTable$sex_1[i]<-srrTable$sex[idx][1]
+    if(length(geoNums)==1){
+      print(paste0("no geo IDs: ",i))
+      failed<-c(failed,i)
+      next;
+    } else if(length(geoNums)>4){
+      print(paste("More than two replicates! did not process:",
+                   paste0(geoNums, collapse=";")))
+      failed<-c(failed,i)
+      next;
+    } else if(length(geoNums)>2){
+      #print(paste("processing 2 replicates",i,paste0(geoNums,collapse=";")))
+      dflines$input_1<-srrTable$SRR[idx][srrTable$input[idx]]
+      dflines$ip_1<-srrTable$SRR[idx][srrTable$ip[idx]]
+      dflines$name_1<-rbind(paste0(srrTable$name[idx][srrTable$rep1[idx]],
+                                     collapse=";"),
+                            paste0(srrTable$name[idx][srrTable$rep2[idx]],
+                                     collapse=";"))
+    } else if(length(geoNums)<=2) {
+      print(paste("processing 1 replicate",i,paste0(geoNums,collapse=";")))
+      dflines$input_1<-srrTable$SRR[idx][srrTable$input[idx]]
+      dflines$ip_1<-srrTable$SRR[idx][srrTable$ip[idx]]
+      dflines$name_1<-paste0(srrTable$name[idx][srrTable$rep1[idx]],
+                               collapse=";")
+    } else {
+      print("failed for unknown reason: ",i)
+      failed<-c(failed,i)
+      next;
+    }
+    dflines$replicate_1<-paste0("rep",1:(length(geoNums)/2))
+    dflines$genotype_1<-srrTable$genotype[idx][1]
+    dflines$strain_1<-srrTable$strain[idx][1]
+    dflines$stage_1<-srrTable$stage[idx][1]
+    dflines$sex_1<-srrTable$sex[idx][1]
+    if(is.null(newTable)){
+      newTable<-dflines
+    } else {
+      newTable<-rbind(newTable,dflines)
+    }
     geoNums<-NULL
   }
-  return(geoTable)
+  print(paste0("failed rows: ",paste(failed,collapse=",")))
+  return(newTable)
 }
 
-fullTable<-mergeGeoSrrTables(geoTable,srrTable)
 
-write.table(fullTable, file="modEncode_chromatinChipSeq_modHistone_fullTable.tsv", sep="\t", row.names=F)
+
+
+
+#fullTable<-mergeGeoSrrTables(geoTable,srrTable)
+
+
+fullTable1<-mergeGeoSrrTables1(geoTable,srrTable)
+write.table(fullTable1, file=paste0(workDir,"/modEncode_chromatinChipSeq_modHistone_fullTable.tsv"), sep="\t", row.names=F)
 
 
--- modEncode_modHistone/makeSRRtable.R
+++ modEncode_modHistone/makeSRRtable.R
@@ -1,14 +1,36 @@
 devStage="L3"
 
+projDir<-getwd()
+workDir<-paste0(projDir,"/modEncode_modHistone")
 
 #########-
 ## Prepare table for chipseq pipeline
 #########-
 
 # make SRR file for datasets that have geo
-chipdata<-read.delim("modEncode_chromatinChipSeq_modHistone_fullTable.tsv")
+chipdata<-read.delim(paste0(workDir, "/modEncode_chromatinChipSeq_modHistone_fullTable.tsv"))
 
 l3<-chipdata[grepl(devStage,chipdata$stage_1) & !is.na(chipdata$name_1),]
+sampleNames<-sapply(strsplit(l3$name_1,";"),"[",1)
+sampleNames<-gsub(paste0("_N2_L3.*$"),"",gsub("^seq-","",sampleNames))
+sampleNames<-gsub(":","_",sampleNames)
+
+cleanMetadata<-data.frame(modEncodeID=gsub("modENCODE_","mE",l3$DCC.id),
+                          strain=l3$strain_1, stage=gsub(" Larva","",l3$stage_1),
+                          sex=gsub("mixed Male and Hermaphrodite population",
+                                   "maleherm",l3$sex_1),
+                          antibody=sapply(strsplit(sampleNames,"_"),"[",1),
+                          target=sapply(strsplit(sampleNames,"_"),"[",2))
+
+sameTargetGroups<-factor(cleanMetadata$target)
+
+SRRs<-data.frame(input=gsub(";"," ",l3$input),
+                 ip=gsub(";"," ",l3$ip),
+                 name=with(cleanMetadata,paste(target,modEncodeID,strain,stage,sex,antibody,sep="_")),
+                 group=as.numeric(sameTargetGroups))
+
+
+write.table()
 
 
 
