03d4e752f93e954196657d61ecc4fab9dad565fe
jsemple19 <semple.j@gmail.com>
2021-03-09 18:53:08 +0100

    Only 4 new samples in modHistone ftp

--- modEncode_modHistone/makeSRRtable.R
+++ modEncode_modHistone/makeSRRtable.R
@@ -1,10 +1,12 @@
+library("tidyverse")
+
 devStage="L3"
 
 projDir<-getwd()
 workDir<-paste0(projDir,"/modEncode_modHistone")
 
 #########-
-## Prepare table for chipseq pipeline
+## Prepare table for chipseq pipeline -----
 #########-
 
 # make SRR file for datasets that have geo
@@ -30,15 +32,69 @@ SRRs<-data.frame(input=gsub(";"," ",l3$input),
                  group=as.numeric(sameTargetGroups))
 
 
-write.table()
+write.table(SRRs,paste0(projDir,"/modEncode_chromatinChipSeq_modHistone.csv"),
+            sep=";",quote=F,row.names=F)
 
 
+alreadyFetched<-cleanMetadata$modEncodeID
+alreadyFetchedSrrs<-c(SRRs$input, SRRs$ip)
+prjna63455<-read.csv2(paste0(projDir,"/SRR_namesPRJNA63455.csv"))
+prjna<-c(unlist(lapply(prjna63455$input,strsplit," ")), unlist(lapply(prjna63455$ip,strsplit," ")))
 
-# get ftp for other data
-ftpdata<-read.table("downloadurls_histoneMod.txt",skip=1,header=F)
-colnames(ftpdata)<-c("ID","url")
+alreadyFetchedSrrs %in% prjna # all included in previous download.
+
+
+#########################-
+# get ftp for other data------
+#########################-
+# load table of urls
+ftpdata<-read.table(paste0(workDir,"/downloadurls_histoneMod.txt"),skip=1,
+                    header=F)
+colnames(ftpdata)<-c("modEncodeID","url")
+
+# get rid of lines for wigs etc.
 idx<-grepl("fastq.gz|fq.gz",ftpdata$url)
 ftpdata<-ftpdata[idx,]
 
-idx<-(gsub("modENCODE_","",chipdata$DCC.id) %in% ftpdata$ID) & is.na(chipdata$name_1)
-chipdata[idx,]
+# reformat the modEncodeID
+ftpdata$modEncodeID<-paste0("mE",ftpdata$modEncodeID)
+
+# remove datasets already downloaded with GEO numbers
+idx<-ftpdata$modEncodeID %in% alreadyFetched
+ftpdata<-ftpdata[!idx,]
+
+#load metadata
+ftpmeta<-read.delim(paste0(workDir,"/metadata_histoneMod.tsv"))
+mEids<-strsplit(ftpmeta$ID,", |and ")
+mEcount<-data.frame(datasetGroup=1:length(mEids), datasetCount=sapply(mEids,length))
+
+mEidTbl<-data.frame(modEncodeID=unlist(mEids),datasetGroup=rep(mEcount$datasetGroup,mEcount$datasetCount))
+mEidTbl$modEncodeID<-gsub(" ","",mEidTbl$modEncodeID)
+mEidTbl<-mEidTbl[!mEidTbl$modEncodeID=="",]
+mEidTbl$modEncodeID<-paste0("mE",mEidTbl$modEncodeID)
+
+match(ftpdata$modEncodeID, mEidTbl$modEncodeID)
+ftpdata<-left_join(ftpdata,mEidTbl,by="modEncodeID")
+ftpdata<-cbind(ftpdata,ftpmeta[ftpdata$datasetGroup,])
+
+# restrict to l3
+idx<-grep("L3",ftpdata$Conditions)
+l3<-ftpdata[idx,]
+
+l3ftpWithSrr<-l3$url[grep("SRR\\d*",l3$url)]
+l3ftpWithSrr<-gsub("\\.fastq\\.gz","",substring(l3ftpWithSrr,regexpr("SRR\\d*",l3ftpWithSrr)))
+l3ftpWithSrr %in% alreadyFetchedSrrs # none have been included
+
+# part of PRJNA63461 project
+prjna63461<-read.csv("/Users/semple/Documents/MeisterLab/Datasets/PRJNA63461_functGenomics/PRJNA63461_modencode_functionalGenomics.txt")
+l3ftpWithSrr %in% prjna63461$Run # yes, for all but three
+l3ftpWithSrr[!l3ftpWithSrr %in% prjna63461$Run] %in% prjna
+# all are  members of PRJNA63455
+
+
+# and the remaining ones?
+# find mE ids of ones with srr in order to remove them
+mEinPrjna<-unique(l3$modEncodeID[grep("SRR\\d*",l3$url)])
+
+l3noSrr<-l3[! l3$modEncodeID %in% mEinPrjna,]
+# only 4 datasets: 5051,5053,5054,5055
