37fb3cf73ef92f1dcab01b18f15bfd9e826fc9dc
jsemple19 <semple.j@gmail.com>
2021-03-10 13:59:52 +0100

    fix Enconde directory name. cleanExcelTable.R adapted to mix chip

--- modEncode_nonHistone/makeSRRtable.R
+++ modEncode_nonHistone/makeSRRtable.R
@@ -41,14 +41,14 @@ alreadyFetchedSrrs<-c(SRRs$input, SRRs$ip)
 prjna63455<-read.csv2(paste0(projDir,"/SRR_namesPRJNA63455.csv"))
 prjna<-c(unlist(lapply(prjna63455$input,strsplit," ")), unlist(lapply(prjna63455$ip,strsplit," ")))
 
-alreadyFetchedSrrs %in% prjna # all included in previous download.
+alreadyFetchedSrrs %in% prjna # all included in previous download PRJNA63455
 
 
 #########################-
 # get ftp for other data------
 #########################-
 # load table of urls
-ftpdata<-read.table(paste0(workDir,"/downloadurls_histoneMod.txt"),skip=1,
+ftpdata<-read.table(paste0(workDir,"/downloadurls_otherChrBind.txt"),skip=1,
                     header=F)
 colnames(ftpdata)<-c("modEncodeID","url")
 
@@ -63,8 +63,11 @@ ftpdata$modEncodeID<-paste0("mE",ftpdata$modEncodeID)
 idx<-ftpdata$modEncodeID %in% alreadyFetched
 ftpdata<-ftpdata[!idx,]
 
+
+
+
 #load metadata
-ftpmeta<-read.delim(paste0(workDir,"/metadata_histoneMod.tsv"))
+ftpmeta<-read.delim(paste0(workDir,"/metadata_otherChrBind.tsv"))
 mEids<-strsplit(ftpmeta$ID,", |and ")
 mEcount<-data.frame(datasetGroup=1:length(mEids), datasetCount=sapply(mEids,length))
 
@@ -76,10 +79,12 @@ mEidTbl$modEncodeID<-paste0("mE",mEidTbl$modEncodeID)
 match(ftpdata$modEncodeID, mEidTbl$modEncodeID)
 ftpdata<-left_join(ftpdata,mEidTbl,by="modEncodeID")
 ftpdata<-cbind(ftpdata,ftpmeta[ftpdata$datasetGroup,])
+table(ftpdata$Conditions)
 
 # restrict to l3
 idx<-grep("L3",ftpdata$Conditions)
 l3<-ftpdata[idx,]
+
 
 l3ftpWithSrr<-l3$url[grep("SRR\\d*",l3$url)]
 l3ftpWithSrr<-gsub("\\.fastq\\.gz","",substring(l3ftpWithSrr,regexpr("SRR\\d*",l3ftpWithSrr)))
@@ -87,9 +92,8 @@ l3ftpWithSrr %in% alreadyFetchedSrrs # none have been included
 
 # part of PRJNA63461 project
 prjna63461<-read.csv("/Users/semple/Documents/MeisterLab/Datasets/PRJNA63461_functGenomics/PRJNA63461_modencode_functionalGenomics.txt")
-l3ftpWithSrr %in% prjna63461$Run # yes, for all but three
-l3ftpWithSrr[!l3ftpWithSrr %in% prjna63461$Run] %in% prjna
-# all are  members of PRJNA63455
+l3ftpWithSrr %in% prjna63461$Run # yes, only one dataset
+#l3ftpWithSrr[!l3ftpWithSrr %in% prjna63461$Run] %in% prjna
 
 
 # and the remaining ones?
@@ -97,6 +101,5 @@ l3ftpWithSrr[!l3ftpWithSrr %in% prjna63461$Run] %in% prjna
 mEinPrjna<-unique(l3$modEncodeID[grep("SRR\\d*",l3$url)])
 
 l3noSrr<-l3[! l3$modEncodeID %in% mEinPrjna,]
-# only 4 datasets: 5051,5053,5054,5055
-# all these samples have the same input with Fxx01175 and Fxx01517 in the name
-# In the url file they have a .txt.gz extension for some reason
+l3noSrr
+# no datasets left
