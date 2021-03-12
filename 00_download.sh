74c613917ee1ac8a603dc346f1f109e47cfd458f
meisterpeter <peter.meister@izb.unibe.ch>
2021-03-12 14:14:49 +0100

    adding checks and retries of fasterq-dump

--- 00_download.sh
+++ 00_download.sh
@@ -7,26 +7,72 @@ module add UHTS/Analysis/sratoolkit/2.10.7;
 SRR_exp=$1
 SRR_IP=($2)
 SRR_input=($3)
-echo $1
-echo $2
-echo $3
-#create folder for SRR download if it does not exists
+nThreads=$4
+
+echo $SRR_exp experiment
+echo $SRR_IP IP
+echo $SRR_input input
+echo $nThreads threads
+#create folder for SRR download if it does not exists and delete content if it does
 [ ! -d $working_path/$SRR_exp/SRR_download ] && mkdir $working_path/$SRR_exp/SRR_download
+rm -rf $working_path/$SRR_exp/SRR_download/*
 [ ! -d $working_path/$SRR_exp/SRR_download/IP ] && mkdir $working_path/$SRR_exp/SRR_download/IP
 [ ! -d $working_path/$SRR_exp/SRR_download/input ] && mkdir $working_path/$SRR_exp/SRR_download/input
-echo "Downloading IP: $SRR_IP"
+
+mkdir -p /scratch/meisterLab
+echo "\nDownloading IP: $SRR_IP"
 for i in "${SRR_IP[@]}"
 do
    echo $i
-   prefetch -O $working_path/$SRR_exp/SRR_download/IP/ -o $working_path/$SRR_exp/SRR_download/$i.srr $i
-   fasterq-dump -O $working_path/$SRR_exp/SRR_download/IP -t ./tmp/ $working_path/$SRR_exp/SRR_download/$i.srr
+   prefetch -o $working_path/$SRR_exp/SRR_download/$i.srr $i
+   vdb-validate $working_path/$SRR_exp/SRR_download/$i.srr
+   if [ $? -ne 0  ]
+   then
+	echo "trying fasterq on its own"
+	fasterq-dump -O $working_path/$SRR_exp/SRR_download/IP -t /scratch/meisterLab/$SRR_exp -e $nThreads $i
+   else
+   	echo "running fasterq with prefetch"
+   	fasterq-dump -O $working_path/$SRR_exp/SRR_download/IP -t /scratch/meisterLab/$SRR_exp -e $nThreads $working_path/$SRR_exp/SRR_download/$i.srr
+   fi
+
+   vdb-validate $working_path/$SRR_exp/SRR_download/IP/${i}.srr.fastq
+   if [ $? -ne 0 ]
+   then
+   	echo "dowload failed. trying fastq-dump"
+	fastq-dump -O $working_path/$SRR_exp/SRR_download/IP $i
+   fi
+
+   echo "compressing fastq with gzip."
+   gzip $working_path/$SRR_exp/SRR_download/IP/${i}.srr.fastq
+ 
 done
-echo "Downloading input: $SRR_input"
+
+
+echo "\nDownloading input: $SRR_input"
 for i in "${SRR_input[@]}"
 do
-   prefetch -O $working_path/$SRR_exp/SRR_download/input -o $working_path/$SRR_exp/SRR_download/$i.srr $i
-   fasterq-dump -O $working_path/$SRR_exp/SRR_download/input -t ./tmp/ $working_path/$SRR_exp/SRR_download/$i.srr
+   prefetch -o $working_path/$SRR_exp/SRR_download/$i.srr $i
+   vdb-validate $working_path/$SRR_exp/SRR_download/$i.srr
+   if [ $? -ne 0  ]
+   then
+       echo "trying fasterq on its own"
+       fasterq-dump -O $working_path/$SRR_exp/SRR_download/input -t /scratch/meisterLab/$SRR_exp -e $nThreads $i
+   else
+       echo "running fasterq with prefetch"
+       fasterq-dump -O $working_path/$SRR_exp/SRR_download/input -t /scratch/meisterLab/$SRR_exp -e $nThreads $working_path/$SRR_exp/SRR_download/$i.srr
+   fi
+   vdb-validate $working_path/$SRR_exp/SRR_download/input/${i}.srr.fastq
+   
+   if [ $? -ne 0 ]
+   then
+   	echo "download failed. trying fastq-dump"
+	fastq-dump -O $working_path/$SRR_exp/SRR_download/IP $i
+   fi
+
+   echo "compressing fastq with gzip."
+   gzip $working_path/$SRR_exp/SRR_download/input/${i}.srr.fastq
 done
+
 echo "This is over"
 
 module rm UHTS/Analysis/sratoolkit/2.10.7;
--- looper.sh
+++ looper.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-#SBATCH --mail-user=peter.meister@izb.unibe.ch
+#SBATCH --mail-user=jennifer.semple@izb.unibe.ch
 #SBATCH --mail-type=end,fail
-#SBATCH --array=2-103%10
+#SBATCH --array=2-34%10
 #SBATCH --job-name="ChIP_seq_modENCODE"
-#SBATCH --time=2-12:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=8G
-#current_path=$PWD
+#SBATCH --output=slurm-%x-%A-%a.out
+#SBATCH --error=slurm-%x-%A-%a.out
+
 echo $PWD
-source file_locations.sh
-working_path="$(dirname "$(pwd)")"
-export working_path=$working_path
+source ./file_locations.sh
+
+mkdir -p $working_path
 echo $working_path
 echo "Line $SLURM_ARRAY_TASK_ID"
-sh wrapper.sh $working_path/SRR_names.csv $SLURM_ARRAY_TASK_ID
+
+sh wrapper.sh $SLURM_ARRAY_TASK_ID $SLURM_CPUS_PER_TASK
 
 
