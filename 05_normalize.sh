e0da30ffb98f4f5c5145cfce440fd4c5645e6021
meisterpeter <peter.meister@izb.unibe.ch>
2021-03-15 14:29:27 +0100

    correct multiqc format

--- 05_normalize.sh
+++ 05_normalize.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 module add R/3.6.1;
+module add UHTS/Analysis/MultiQC/1.8;
+
 SRR_exp=$1
 [ ! -d $working_path/$SRR_exp/norm ] && mkdir $working_path/$SRR_exp/norm
 [ ! -d $working_path/$SRR_exp/enrichment ] && mkdir $working_path/$SRR_exp/enrichment
+
+multiqc -i allRuns -o $working_path/qc $working_path
 
 Rscript 05_normalize.R "$working_path/$SRR_exp"
